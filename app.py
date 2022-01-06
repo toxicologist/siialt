bc42cce61c9f4f0a7c519bc06e960d1a7b6e7173
toxicologist <toxiccologist@gmail.com>
2022-01-06 15:39:31 -0300

    coisas novas

--- app.py
+++ app.py
@@ -8,4 +8,6 @@ def index():  # put application's code here
 
 
 if __name__ == '__main__':
-    app.run()
+    app.jinja_env.auto_reload = True
+    app.config['TEMPLATES_AUTO_RELOAD'] = True
+    app.run(debug=True)
